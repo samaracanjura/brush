1a17d3b44e5df6c13fd5fb4b5b490c00eaf9eb8c
jenny han <jlhan@stanford.edu>
2021-07-07 14:35:53 -0700

    original template

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
-let colorlist = ['gold', 'yellow', 'turquoise', 'red']
 
+let brushCounterLimit = 5;
+let brushCounter = 1;
+
+let images;
+
+function preload(){
+  images = [
+    loadImage('assets/doodle_1.png'),
+    loadImage('assets/doodle_2.png'),
+    loadImage('assets/doodle_3.png'),
+    loadImage('assets/doodle_4.png'),
+    loadImage('assets/doodle_5.png'),
+    loadImage('assets/doodle_6.png'),
+    loadImage('assets/doodle_7.png')
+  ]
+}
 function setup() {
   createCanvas(windowWidth, windowHeight);
-    background(255);
+    background("#DEF0E5");
 }
 
 function draw() {
-  noStroke();
-  fill(random(colorlist));
-  ellipse(mouseX, mouseY, 25, 25);
+  if(mouseIsPressed){
+    incrementBrushCounter();
+    if (brushCounter == 0){
+    tint(255, random(50, 200));
+    image(random(images), mouseX, mouseY, 60, 60);
+    }
+  }
+}
+
+function incrementBrushCounter(){
+  brushCounter++;
+  if(brushCounter > brushCounterLimit){
+    brushCounter = 0;
+  }
 }
